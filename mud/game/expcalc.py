bd795d2dc439ab365d5753a1903214059e20debb
Vincent Zeng <hvincent@modgethanc.com>
2015-03-06 04:22:12 -0500

    added exp value cache

--- mud/game/expcalc.py
+++ mud/game/expcalc.py
@@ -1,8 +1,9 @@
+cached = [0,100] #base case, arbitrary level 1 base exp
+
 def expFor(level):
-    if level == 0:
-        return 0 #  base case
-    elif level == 1:
-        return 100 # arbitrary base exp
+    if len(cached)-1 >= level:
+        return cached[level]
     else:
         factor = 1.2 #arbitrary factor
-        return int((expFor(level-1)-expFor(level-2))*factor + expFor(level-1))
+        cached.append(int((expFor(level-1)-expFor(level-2))*factor + expFor(level-1)))
+        return cached[level]
